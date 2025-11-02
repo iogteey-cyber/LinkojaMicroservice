cf824d0acc1e03d283d65b67adea11eff020a32d
iogteey-cyber <iogteey@gmail.com>
2025-11-02 20:19:25 +0100

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace LinkojaMicroservice.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        
+        public string Phone { get; set; }
+        
+        [Required]
+        public string PasswordHash { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Role { get; set; } = "user"; // user, business_owner, admin
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
